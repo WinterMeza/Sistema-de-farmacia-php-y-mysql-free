679a90b40aa4727cfa0549b2109f41e37292708e
WinterMeza <e1317233748@live.uleam.edu.ec>
2023-09-17 03:05:28 -0500

    i

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
     $host = "localhost";
-    $user = "root";
-    $clave = "";
+    $user = "wintermeza";
+    $clave = "Wamj1317233748@";
     $bd = "v_farmacia";
     $conexion = mysqli_connect($host,$user,$clave,$bd);
     if (mysqli_connect_errno()){
